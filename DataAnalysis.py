473833aabcc90d455d642e834952b73554d3ebe5
Cristiano Aguiar <cristiano@b206.com>
2016-05-27 17:14:13 -0400

    Second commit

--- DataAnalysis.py
+++ DataAnalysis.py
@@ -7,6 +7,7 @@ Created on Fri May 20 17:27:36 2016
 import pandas as pd
 import numpy as np
 import seaborn as sns
+import matplotlib.pyplot as plt
 from sklearn.cluster import KMeans
 import locale
 locale.setlocale( locale.LC_ALL, 'en_US.UTF-8' ) 
@@ -83,10 +84,72 @@ sns.plt.show()
 
 # LO.PISA.REA and  LO.PISA.SCI seems to be very dependent, let's use only LO.PISA.REA
 # To cluster our data
-clustering_data = merged_data[['LO.PISA.REA','CPI2015','GDP.Norm','HDI.Index']]
-# 
-kmeans_model = KMeans(n_clusters=3, random_state=1)
-countries_distances = kmeans_model.fit_transform(clustering_data)
-countries_labels = kmeans_model.labels_
-merged_data["cluster"] = countries_labels
+# Let's group them into 3 clusters only
+kmeans_studies = {}
+kmeans_studies['all_data'] = {}
+kmeans_studies['all_data']['variables'] = ['LO.PISA.REA','CPI2015','GDP.Norm','HDI.Index']
+
+# Educational Data and Corruption
+kmeans_studies['edu_cpi'] = {}
+kmeans_studies['edu_cpi']['variables'] = ['LO.PISA.REA','CPI2015']
+
+# Human development index and corruption
+kmeans_studies['hdi_cpi'] = {}
+kmeans_studies['hdi_cpi']['variables'] = ['HDI.Index','CPI2015']
+
+# Loop over models
+for k in kmeans_studies.keys():
+    kmeans_studies[k]['model'] = KMeans(n_clusters=3)
+    distances = kmeans_studies[k]['model'].fit_transform(merged_data[kmeans_studies[k]['variables']])
+    distances = pd.DataFrame(distances).min(axis=1)
+    kmeans_studies[k]['distances'] = distances    
+    kmeans_studies[k]['labels'] = kmeans_studies[k]['model'].labels_
+    merged_data[k] = kmeans_studies[k]['labels']
+    merged_data[k+'_dist'] = kmeans_studies[k]['distances']
+    
+   
+plt.scatter(merged_data['LO.PISA.REA'],merged_data['CPI2015'],c=merged_data['edu_cpi'])
+plt.title("PISA vs. CPI")
+plt.xlabel("PISA")
+plt.ylabel("CPI")
+plt.show()
+
+plt.scatter(merged_data['CPI2015'],merged_data['HDI.Index'],c=merged_data['hdi_cpi'])
+plt.title("CPI vs. HDI")
+plt.xlabel("CPI")
+plt.ylabel("HDI")
+plt.show()
+
+plt.scatter(merged_data['CPI2015'],merged_data['HDI.Index'],c=merged_data['all_data'])
+plt.show()
+
+# Bar
+width = 5
+gap = 3
+y_countries = np.arange(len(merged_data['Country']))*(width+gap)
+
+table_hdi_cpi = merged_data[['Country','HDI.Index','CPI2015','hdi_cpi']]
+table_hdi_cpi['HDI.Index'] = table_hdi_cpi['HDI.Index']/table_hdi_cpi['HDI.Index'].max()
+table_hdi_cpi['CPI2015'] = table_hdi_cpi['CPI2015']/table_hdi_cpi['CPI2015'].max()
+table_hdi_cpi.sort_values(by=['hdi_cpi'],ascending=True,inplace=True)
+table_hdi_cpi['y_countries'] = y_countries
+table_max_y = table_hdi_cpi.groupby('hdi_cpi').max()
+table_max_y = table_max_y[['y_countries']]
+
+fig, ax = plt.subplots(figsize=(10, 16))
+ax.barh(y_countries,table_hdi_cpi['HDI.Index'],height=5,color = '#3366FF')
+ax.barh(y_countries,table_hdi_cpi['CPI2015'],height=5,left=table_hdi_cpi['HDI.Index'], color = '#FF6666')
+ax.yaxis.set_ticks(y_countries+width/2)
+ax.set_yticklabels(table_hdi_cpi['Country'])
+ax.set(ylim=(np.min(y_countries)-width,np.max(y_countries)+width))
+ax.axhline(table_max_y.iloc[0,0]+width+gap/2, color='b', linestyle='dashed', linewidth=2)
+ax.axhline(table_max_y.iloc[1,0]+width+gap/2, color='b', linestyle='dashed', linewidth=2)
+ax.axhline(table_max_y.iloc[2,0]+width+gap/2, color='b', linestyle='dashed', linewidth=2)
+
+
+plt.tight_layout()
+fig.savefig("hdi_cpi.png")
+fig.clf()
+
+
 
